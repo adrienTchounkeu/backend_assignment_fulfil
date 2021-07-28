1965c034c2f78c17a50900b4fe0d0c1e683b2c8d
Adrien Tchounkeu <adrientchounkeu10@gmail.com>
2021-07-28 01:05:23 +0100

    add the User and Product Model

--- backend_assignment/models.py
+++ backend_assignment/models.py
@@ -1,5 +1,16 @@
 from django.db import models
 
 
+# basic user schema
 class User(models.Model):
     name = models.CharField(max_length=500)
+    create_trigger = models.BooleanField(default=False)
+    update_trigger = models.BooleanField(default=False)
+
+
+# product schema
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    sku = models.CharField(max_length=40, unique=True)
+    description = models.CharField(max_length=500)
+    status = models.BooleanField(default=False)
